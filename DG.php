d07484eea6f020648853b545e6d5d432796e992f
Ulfsark_Rage <nikolasmurcia80@gmail.com>
2023-03-31 20:31:10 -0500

    Cambios en CSS
    Se agrega GNMB

--- DG.php
+++ DG.php
@@ -9,36 +9,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="Materials/LilMC.png" />
+    <link rel="stylesheet" href="css/Estilos.css">
     <script src="../js/jquery-3.6.0.min.js"></script>
-    <title>Decoder</title>
+    <title>Generador de documentos</title>
 </head>
 
-<style>
-    header {
-        background-color: black;
-        height: 55px;
-        display: flex;
-        justify-content: space-between;
-    }
-    .headerlogo {
-        display: flex;
-        justify-content: flex-end;
-        margin: 0% 0.5%;
 
-    }
-    
-    .headerlogo a{
-        color: white;
-        text-decoration: none;
-        font-weight: bold;
-
-    }
-    .headerlogo img{
-        height:5vh; 
-        margin:2% 0;
-
-    }
-</style>
 <header>
 
     <?php
@@ -51,119 +27,12 @@
         <a href="../inicio2.php"><img src="Materials/LilMC.png" alt=""></a>
     </div>
     <div class="headerlogo">
-        <img src="../Materials/DevlabWhite.png" alt="">
+        <img src="Materials/GNMB.png" alt="">
     </div>
 
 </header>
 
 
-<style>
-    @font-face {
-        
-        font-family: "Sancoale";
-        src: url('../fuentes/SancoaleSoftenedMedium.otf');
-    }
-    body{
-        font-family: 'Sancoale', sans-serif;
-        margin: 0;
-        min-height: 100%;
-        background-image: url(../Materials/FondoestaticoWH.png);
-        /*background-repeat: no-repeat;
-        background-size: cover;*/
-        overflow: scroll;
-        background-attachment: fixed;
-   
-
-    }
-
-    .busqueda {
-        width: 90%;
-        resize: none;
-        height: 40vh;
-        
-        
-    }
-
-    #b1 {
-        display: block;
-        width: 100%;
-        height: 48vh;
-        margin-top: 10%;
-        background-color: rgba(250, 0, 67, 0.1);
-        border-radius: 1rem;
-    }
-    #b2  {
-        display: none;
-        width: 100%;
-        height: 48vh;
-        margin-top: 10%;
-        background-color: rgba(250, 0, 67, 0.1);
-        border-radius: 1rem;
-    }
-    #b3 {
-        display: none;
-        width: 100%;
-        height: 48vh;
-        margin-top: 10%;
-        background-color: rgba(250, 0, 67, 0.1);
-        border-radius: 1rem;
-    }
-    #b4 {
-        display: none;
-        width: 100%;
-        height: 48vh;
-        margin-top: 10%;
-        background-color: rgba(250, 0, 67, 0.1);
-        border-radius: 1rem;
-    }
-    .containerp {
-        margin-top: 5%;
-        display: flex;
-    }
-    #decoder{
-        max-width: 100%;
-    }
-    .login-container {
-        margin: 0% 3%;
-        min-width: 20%;
-        max-width: 20.5%;
-    }
-    .selector {
-        margin: 5%;
-       
-    }
-    #selector1 {
-        height: 25px;
-        width: 90%;
-        font-weight: bold;
-        font-family: 'Sancoale', sans-serif;
-        font-size: 110%;
-
-    
-    }
-    h3 {
-        color: red;
-        margin-bottom: 1%;
-        margin-left: 5%;
-        height: 25px;
-        text-shadow: whitesmoke 2px 2px;
-        font-size: 170%;
-        font-weight: bold;
-        display: inline-flex;
-        margin-top: 8px;
-    }
-
-    .boton {
-        font-family: 'Sancoale', sans-serif;
-        background-color: rgba(250, 0, 67, 0.2);
-        border-radius: 1rem;
-        width: 100%;
-
-    }
-    .contbtn {
-        width: 45%;
-    }
-</style>
 
 <body>
     <div class="containerp" >
